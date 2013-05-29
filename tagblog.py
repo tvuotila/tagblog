e69c3cc785e35db8a708b13a50cff74f70b6b79d
Tero Vuotila <tero.vuotila@gmail.com>
2013-05-30 02:09:25 +0300

    Added Database connection
    Added Database connection and test code for it.

--- tagblog.py
+++ tagblog.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, Markup, abort, redirect, url_for, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
+db = SQLAlchemy(app)
 
+# Class for testing database connection
+# Contains number of visits to a site.
+class Visitor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer)
 
+# Create database
+db.create_all()
+
+# HelloWorld for testing site
 @app.route('/')
 def helloworld():
-    return render_template('hello.html')
+    visit = Visitor.query.first()
+    if visit == None:
+        visit = Visitor()
+        visit.number = 0
+    visit.number = visit.number + 1
+    db.session.add(visit)
+    db.session.commit()
+    return render_template('hello.html', visitors=visit.number)
 
 
 if __name__ == '__main__':
